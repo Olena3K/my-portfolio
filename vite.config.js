cda3c40161f18f82b41bcbfa91435bca6b7de0b9
Olena3K <olenakukhar3@gmail.com>
2025-07-30 16:41:18 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ghPages from "vite-plugin-gh-pages";
+
+const repo = "my-portfolio";
 
 export default defineConfig({
-  plugins: [react()],
-})
+  base: `/${repo}/`,
+  plugins: [react(), ghPages()],
+});
